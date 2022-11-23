eda4462a1cae7014118997b2ab6c31c7c99b139e
Spanyk <paulo.augusto62@hotmail.com>
2022-11-22 23:34:47 -0300

    Add navmenu

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 include_once 'header.php';
-include_once 'aut/login.php';
+// include_once 'aut/login.php';
+
 ?>
+<nav class="navbar-container">
+    <div class="navbar-milenium-banner">
+        <a href="index.php"><img src="/assets/img/milenium-filmes-banner.png"></a>
+    </div>
+</nav>
 
 
 <?php include_once 'footer.php'?>
